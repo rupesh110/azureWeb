3bbd486531cfb0552c00decf169277bc7d91baaa
Rupesh <shrestharupesh110@gmail.com>
2023-12-18 10:45:37 +1100

    minor fixes

--- azurefunction/src/functions/httpTrigger1.js
+++ azurefunction/src/functions/httpTrigger1.js
@@ -16,7 +16,7 @@ app.http('httpTrigger2', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
+        context.log(`Http function processed request for url here "${request.url}"`);
 
         const name = request.query.get('name') || await request.text() || 'Again';
 
